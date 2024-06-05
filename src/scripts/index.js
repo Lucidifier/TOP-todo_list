02557c540f14dcfa7462089f6e57445b2450d773
Lucidifier <dragan.hardi3@gmail.com>
2024-06-05 14:54:59 +0200

    setup webpack

---
+++ src/scripts/index.js
@@ -0,0 +1,11 @@
+import '../styles/style.css';
+
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = 'hello';
+  element.classList.add('hello');
+  return element;
+}
+
+document.body.appendChild(component());
